d6b7a138bfa9e9ee4e21dc8ecbcd40b6f27372c7
AUBENAS Mike <mike.aubenas@gmail.com>
2022-07-11 23:36:40 +0200

    feat: help flag

--- src/core/ArgHandler.ts
+++ src/core/ArgHandler.ts
@@ -2,6 +2,9 @@ import { System } from "./System"
 
 import { FirstArgError } from "../errors/CLISyntaxErrors"
 import { FileAlreadyExistsError, FileNotFoundError } from "../errors/FileErrors"
+import { printMessage } from "./Printer"
+import Help, { DEFAULT_CREATE_EXTENSION } from "./Help"
+import { handleExtractExtension } from "../utils"
 
 //////////////////// ////////////////////
 
@@ -13,6 +16,17 @@ export enum Action {
 export const ARCHIVE_ALGO = 'tgz'
 export const ENCRYPTION_ALGO = 'aes-256-cbc'
 
+
+export enum Flag {
+	HELP = '--help'
+}
+
+interface ArgOccurance
+{
+	arg: string
+	index: number
+}
+
 //////////////////// ////////////////////
 
 export class ArgHandler {
@@ -23,8 +37,18 @@ export class ArgHandler {
 	sourcePath: string
 	destPath: string
 
+	isHelpNeeded: boolean
+
 	constructor() {
 		this.args = process.argv.slice( 2 )
+
+		// Flags should be handled before positional args
+		this.isHelpNeeded = this.searchFlag([ '-h', Flag.HELP ])
+
+		if(this.isHelpNeeded || this.args[0] === undefined) {
+			printMessage(Help.fullMan())
+			System.exit(0)
+		}
 
 		this.setAction(this.args[0])
 		this.setSource(this.args[1])
@@ -51,11 +75,47 @@ export class ArgHandler {
 	}
 
 	private setDest = (arg: string) => {
-		const path = arg || `${this.sourcePath}.${this.action === Action.CREATE ? 'enc' : 'dec'}`
+		const path = arg ||
+			this.action === Action.CREATE
+			? this.sourcePath + DEFAULT_CREATE_EXTENSION
+			: handleExtractExtension(this.sourcePath)
+
 
 		if( System.doesFileExists(path) )
 			throw new FileAlreadyExistsError(path)
 
 		this.destPath = path
+	}
+
+	////////////////////
+
+	private searchFlag = (flagWrittings: string[]): boolean => {
+		const occurances = this.extractOccurances(arg => flagWrittings.includes(arg))
+
+		return occurances !== undefined
+	}
+
+	/**
+	 * Find occurances in input order with their position that math the callback,
+	 * returns them and delete them from the untreatedArgs array
+	 * If none find, returns undefined
+	 */
+	private extractOccurances = (callback : (arg: string, index ?: number) => boolean ): ArgOccurance[] | undefined =>
+	{
+		const occurances: ArgOccurance[] = []
+
+		this.args.forEach((arg, index) => {
+			if(callback(arg, index))
+				occurances.push({ arg, index })
+		})
+
+		if( occurances.length === 0 )
+			return undefined
+
+		/** @see: https://www.codegrepper.com/code-examples/javascript/array+splice+in+for+loop+javascript */
+		for (let i = occurances.length - 1; i >= 0; i--)
+			this.args.splice(occurances[ i ].index, 1);
+
+		return occurances
 	}
 }
